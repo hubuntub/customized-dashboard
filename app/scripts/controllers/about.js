4962393a527820c56e6e1a4b882f6de934f0aefa
houbeb <houbeb.benothmane@argus-presse.fr>
2016-02-09 13:00:48 +0100

    [INIT] Using Yeoman, angular isotope.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name customizedDashboardApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the customizedDashboardApp
+ */
+angular.module('customizedDashboardApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name customizedDashboardApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the customizedDashboardApp
+ */
+angular.module('customizedDashboardApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
